a0b5f199e21bd4680529a6eb882b007eeaf16f30
guilherme-branchieri <branchieri.contato@gmail.com>
2023-09-05 22:43:19 -0300

    refactor: implementando pipe de validação do zod

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,16 +1,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Controller, Post, HttpCode, Body, ConflictException } from "@nestjs/common";
+import { Controller, Post, HttpCode, Body, ConflictException, UsePipes } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { hash } from "bcryptjs"
+import { z } from "zod"
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 
-@Controller("/accounts/")
+const CreateAccountBodySchema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8),
+    image: z.string(),
+    phone: z.string().min(11).max(11),
+    adress: z.string(),
+    cep: z.string()
+
+})
+
+type CreateAccountBodySchema = z.infer<typeof CreateAccountBodySchema>
+
+
+@Controller("/accounts")
 export class CreateAccountController {
     constructor(private prisma: PrismaService) { }
 
     @Post()
     @HttpCode(201)
-    async handle(@Body() body: any) {
-        const { firstName, lastName, email, password, image, phone, adress, cep } = body;
+    @UsePipes(new ZodValidationPipe(CreateAccountBodySchema))
+    async handle(@Body() body: CreateAccountBodySchema) {
+
+        const { firstName, lastName, email, password, image, phone, adress, cep } = CreateAccountBodySchema.parse(body);
         const emailAlreadyRegistered = await this.prisma.user.findUnique({
             where: {
                 email
