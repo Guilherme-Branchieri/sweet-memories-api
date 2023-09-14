6eaff849c0810679f20cb9904e1fbbdb80d4b7ea
guilherme-branchieri <branchieri.contato@gmail.com>
2023-09-14 13:46:15 -0300

    refactor(auth): mudando configs de autenticaçao

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,5 +1,5 @@
-import { PipeTransform, BadRequestException } from "@nestjs/common";
-import { ZodError, ZodSchema } from "zod";
+import { PipeTransform, BadRequestException } from "@nestjs/common"
+import { ZodError, ZodSchema } from "zod"
 import { fromZodError } from "zod-validation-error"
 
 export class ZodValidationPipe implements PipeTransform {
@@ -7,17 +7,19 @@ export class ZodValidationPipe implements PipeTransform {
 
     transform(value: unknown) {
         try {
-            this.schema.parse(value);
+            this.schema.parse(value)
+
         } catch (error) {
             if (error instanceof ZodError) {
                 throw new BadRequestException({
-                    errors: fromZodError(error),
+                    message: "Validation failed",
                     statusCode: 400,
-                    message: "Validation failed"
+                    errors: fromZodError(error),
                 })
             }
-            throw new BadRequestException("Validation failed");
+
+            throw new BadRequestException("Validation failed")
         }
-        return value;
+        return value
     }
 }
