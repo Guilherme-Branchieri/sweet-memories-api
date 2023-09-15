155349801da3cefa6bdca4faf23e96a1d69f9cbb
guilherme-branchieri <branchieri.contato@gmail.com>
2023-09-15 00:48:31 -0300

    refact(clean): limpando código de comentarios desnecessarios

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,14 +3,5 @@ import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private jwtService: JwtService
-    ) { }
-
-    // async login(user: any) {
-    //     // const payload = { username: user.username, sub: user.userId };
-    //     // return {
-    //     //     access_token: this.jwtService.sign(payload),
-    //     // };
-    // }
+    constructor(private jwtService: JwtService) { }
 }
