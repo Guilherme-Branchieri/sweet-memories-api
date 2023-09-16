4f9917ba9bb4ef6a8d1a95e0b2e460b4cbb56dea
guilherme-branchieri <branchieri.contato@gmail.com>
2023-09-16 15:09:24 -0300

    docs: implementando swagger para documentação da api

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { ConfigService } from "@nestjs/config"
 import { Env } from "./env"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -9,7 +10,17 @@ async function bootstrap() {
   const configService: ConfigService<Env, true> = app.get(ConfigService)
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const port = configService.get("PORT", {infer: true})
+  const port = configService.get("PORT", { infer: true })
+
+  const docs = new DocumentBuilder()
+    .setTitle("Sweet Memories")
+    .setDescription("TODO")
+    .setVersion("1.0")
+    .addTag("api")
+    .build();
+  const document = SwaggerModule.createDocument(app, docs);
+  SwaggerModule.setup("api", app, document);
+
 
   await app.listen(port)
 }
