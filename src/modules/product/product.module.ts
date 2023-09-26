00f4ade32e69755c55af3467c268cae237ff608d
guilherme-branchieri <branchieri.contato@gmail.com>
2023-09-26 00:51:41 -0300

    refact(PRODUCT): editando serviço de criação de produto

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common"
-import { APP_GUARD } from "@nestjs/core"
-import { RolesGuard } from "@/common/guards/roles.guard"
-import { CreateProductController } from "./controllers/create.controller"
+import { CreateProductController } from "./controllers/create.product.controller"
 import { AuthService } from "../auth/auth.service"
+import { FindAllProductController } from "./controllers/find-all.product.controller"
 
 @Module({
     imports: [],
-    controllers: [CreateProductController],
+    controllers: [CreateProductController, FindAllProductController],
     providers: [AuthService],
 })
 export class ProductModule { }
---
+++ src/modules/product/use-cases/create.product.spec.ts
@@ -0,0 +1,47 @@
+import { AppModule } from "@/app.module";
+import { INestApplication } from "@nestjs/common";
+import { InMemoryProductsRepository } from "../repositories/in-memory/in-memory-products.repository";
+import { CreateProductUseCase } from "./create.product";
+import { Test } from "@nestjs/testing/test";
+
+
+describe("Create Product Use Case", () => {
+
+    let app: INestApplication
+    let productsRepository: InMemoryProductsRepository
+    let createProductUseCase: CreateProductUseCase
+
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [AppModule],
+        })
+            .compile();
+
+        app = moduleRef.createNestApplication();
+        await app.init();
+    });
+
+    beforeEach(async () => {
+        productsRepository = new InMemoryProductsRepository()
+        createProductUseCase = new CreateProductUseCase(productsRepository)
+    })
+
+    it("Shoud be able to create a new product", async () => {
+        const mockProduct = {
+            id: "test01",
+            name: "Difusor de teste",
+            price: 4.50,
+            description: "Lindo difusor pra testar essa funcionalidade de criação",
+            images: ["wwww.minha-imagem.com"],
+            available: true,
+            category: "batizado"
+        }
+        const { product } = await createProductUseCase.execute(mockProduct)
+        expect(product).toEqual(expect.any(Object));
+        expect(product.id).toEqual(expect.any(String))
+        expect(product).toHaveProperty("price")
+        expect(product.available).toBeTruthy()
+        expect(product.updatedAt).toBe(null || undefined)
+    })
+
+})
